5dd2f45b9faf6528678bffc355ffa27c66108d54
kenandcrys <kenandcrys@live.com>
2023-07-14 21:05:20 -0500

    map state to props

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,10 +1,17 @@
 import React from 'react';
-
+import { connect } from 'react-redux';
 import MovieListItem from './MovieListItem';
 import MovieFooter from './MovieFooter';
 
+
+const mapStateToProps = state => {
+    return {
+        movies: state.movies,
+
+    }
+}
+
 const MovieList = (props)=> {
-    const movies = [];
 
     return (
         <div className="col">
@@ -21,14 +28,14 @@ const MovieList = (props)=> {
 
                 <tbody>
                     {
-                        movies.map(movie=><MovieListItem key={movie.id} movie={movie}/>)
+                        props.movies.map(movie=><MovieListItem key={movie.id} movie={movie}/>)
                     }
                 </tbody>
             </table>
             
-            <MovieFooter totalMovies={movies.length}/>
+            <MovieFooter totalMovies={props.movies.length}/>
         </div>
     );
 }
 
-export default MovieList;
+export default connect(mapStateToProps, {})(MovieList)
