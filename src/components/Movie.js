daa6daf3c9d1900d01b5da9785c169ba48a8131b
kenandcrys <kenandcrys@live.com>
2023-07-19 04:22:47 -0500

    add functionality

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -52,7 +52,7 @@ const Movie = (props) => {
                         </section>
                         
                         <section>
-                            <span className="m-2 btn btn-dark">Favorite</span>
+                            {displayFavorites &&<span className="m-2 btn btn-dark">Favorite</span>}
                             <span className="delete"
                              onClick={handleDelete} ><input type="button" className="m-2 btn btn-danger" value="Delete"/></span>
                         </section>
--- src/reducers/favoritesReducer.js
+++ src/reducers/favoritesReducer.js
@@ -1,13 +1,31 @@
+ import { TOGGLE_FAVORITES, ADD_FAVORITE, REMOVE_FAVORITE } from './../actions/favoritesActions';
+
 const initialState = {
     favorites: [],
     displayFavorites: true
 }
 
 const favoriteReducer = (state = initialState, action) => {
-    switch(action.type){
-        default:
-            return state
+    switch (action.type) {
+      case TOGGLE_FAVORITES:
+        return {
+          ...state,
+          displayFavorites: !state.displayFavorites,
+        };
+      case ADD_FAVORITE:
+        return {
+          ...state,
+          favorites: [...state.favorites, action.payload],
+        };
+      case REMOVE_FAVORITE:
+        return {
+          ...state,
+          favorites: state.favorites.filter(item => item.id !== action.payload),
+        };
+      default:
+        return state;
     }
-}
-
-export default favoriteReducer
+  };
+  
+  export default favoriteReducer;
+  
